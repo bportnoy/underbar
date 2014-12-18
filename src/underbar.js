0ded9210426b28d896305fb1492e04d8d575a2d4
Bradley Portnoy <bradley.portnoy@gmail.com>
2014-12-17 22:52:21 -0800

    Complete extend, some work for defaults.

--- src/underbar.js
+++ src/underbar.js
@@ -248,11 +248,25 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i = 0; i<arguments.length; i++){
+      var arg = arguments[i];
+      for (var key in arg){
+        obj[key] = arg[key];
+      }
+    }
+   return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+     /*for (var i = 0; i<arguments.length; i++){
+      var arg = arguments[i];
+      for (var key in arg){
+        if (!key in obj) obj[key] = arg[key];
+      }
+    }
+   return obj;*/
   };
 
 
